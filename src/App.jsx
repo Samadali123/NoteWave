f4a444494b0eb23d939a2b3c4a2e946463d72a4f
syed samad ali <samadali0125@gmail.com>
2025-05-26 11:38:47 +0530

    Add Redux Toolkit in project and createSlices

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import config from './config/config'
+// import React from 'react'
+// import config from './config/config'
 
-const App = () => {
-  return (
-    <div>Blog app with appwrite </div>
-  )
+// const App = () => {
+//   return (
+//     <div>Blog app with appwrite </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
